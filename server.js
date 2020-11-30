4febc18779077cfa50a1f82e90cef5f7ee416546
naveenzerobugz@gmail.com.com <naveenzerobugz@hotmail.com>
2020-11-30 12:20:03 +0530

    updated website templates

---
+++ server.js
@@ -0,0 +1,18 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/multikart/browser'));
+
+app.get('/*', function(req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/multikart/browser/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 3004, () => {
+    console.log("Server is listening on port  3004");
+});
